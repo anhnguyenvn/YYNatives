f5c72c7d76b516e96eb3f98d16115c7f7334a449
Anh Nguyen <anhnguyenvn@live.com>
2020-04-14 20:18:45 +0700

    Init native tools with GooglePlayGames

---
+++ Scripts/GGHelper.cs
@@ -0,0 +1,38 @@
+﻿using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
+using UnityEngine;
+using System;
+
+public class GGHelper : MonoBehaviour
+{
+    static GGHelper Instance;
+
+
+    void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        InitGGService();
+    }
+
+    void InitGGService()
+    {
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+    }  
+    
+    public void Authenticate(SignInInteractivity signMode, Action cb)
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
